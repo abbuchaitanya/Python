6d5d1e5ddacdc5956241e55b1624b5843634809a
Chaitanya Abbu <Chaitanya.Abbu@vistajet.com>
2020-04-30 23:15:47 +0100

    If statement file committed

---
+++ If_statements.py
@@ -0,0 +1,13 @@
+is_male = True
+
+if is_male:
+    print("You are a Male")
+else:
+    print("You are not a Male")
+
+is_tall = True
+
+if is_male or is_tall:
+    print("You are a Male or You are a tall or You are Both")
+else:
+    print("You are neither of them")
