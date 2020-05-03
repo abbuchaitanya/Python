312dd5d2a4367778c70d2d6e7afd53ac5e36956c
Chaitanya <abbuchaitanya@gmail.com>
2020-05-03 13:59:26 +0100

    Chef_Class.py commit to local repository

---
+++ Chef_Class.py
@@ -0,0 +1,8 @@
+class chef:
+    
+    def biriyani(self):
+        print("Chef makes Biriyani")
+    def pulav(self):
+        print("Chef makes vegetable pulav")
+    def sweet(self):
+        print("Chef makes carrot halwa")
